8c640727d25d443d05fedfd019b88928a44b89db
Felj Abas <felabas41@gmail.com>
2021-03-25 15:39:46 -0700

    fixed error with server.js

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV} = require('./config')
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV=== 'production') ? 'tiny' : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
 app.get('/', (req,res) => {
-    res.send("Hello, world!")
+    res.send("Hello, diva!")
 })
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if(NODE_ENV === 'production') {
+    if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' }}
     } else {
         console.error(error)
