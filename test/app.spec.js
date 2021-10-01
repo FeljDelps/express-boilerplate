5b2e37de97403837e2fbccb35264b28197032fbf
Felj Abas <felabas41@gmail.com>
2021-10-01 14:56:37 -0700

    small change to app component and testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,10 @@
 const app = require('../src/app');
 
 describe('App', () => {
-    it('GET / responds with 200 containing "Hello, world!"', () => {
+    it('GET / responds with 200 containing "Hello, diva!"', () => {
         return supertest(app)
             .get('/')
-            .expect(200, "Hello, world!");
+            .expect(200, "Hello, diva!");
     });
 });
 
